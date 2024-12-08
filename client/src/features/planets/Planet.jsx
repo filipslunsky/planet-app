bf03bddadf60eaae15588bddd55a1294a3b2122a
filipslunsky <fslunsky@gmail.com>
2024-12-08 03:17:30 +0100

    classNames added

--- client/src/features/planets/Planet.jsx
+++ client/src/features/planets/Planet.jsx
@@ -4,6 +4,7 @@ import './planet.css';
 const Planet = ({data}) => {
     const [content, setContent] = useState('overview');
     const [img, setImg] = useState('planet');
+    const [selected, setSelected] = useState(1);
     const PLANETS_URL = `${import.meta.env.VITE_API_URL}`;
 
     const setOption = (optionNumber) => {
@@ -11,14 +12,17 @@ const Planet = ({data}) => {
             case 1:
                 setContent('overview');
                 setImg('planet');
+                setSelected(1);
                 break;
             case 2:
                 setContent('structure');
                 setImg('internal');
+                setSelected(2);
                 break;
             case 3:
                 setContent('geology');
                 setImg('geology');
+                setSelected(3);
                 break;
             default:
                 break;
@@ -26,30 +30,38 @@ const Planet = ({data}) => {
     };
     
     return (
-        <>
-            <img src={`${PLANETS_URL}${data.images[img].substring(1)}`} alt={`${data.name} image`} />
-            <h1>{data.name.toUpperCase()}</h1>
-            <p>{data[content].content}</p>
-            <p>Source: <a href={data[content].source} target='_blank'>Wikipedia &#x2197;</a></p>
-            <h3 onClick={() => {setOption(1)}}>O1 OVERVIEW</h3>
-            <h3 onClick={() => {setOption(2)}}>O2 INTERNAL STRUCTURE</h3>
-            <h3 onClick={() => {setOption(3)}}>O3 SURFACE GEOLOGY</h3>
+        <> 
+            <div className="dynamicDataContainer">
+                <div className="imgAndDescriptionContainer">
+                    <img className='planetImage' src={`${PLANETS_URL}${data.images[img].substring(1)}`} alt={`${data.name} image`} />
+                    <div className="descriptionContainer">
+                        <h1 className='planetName'>{data.name.toUpperCase()}</h1>
+                        <p className='planetDescription'>{data[content].content}</p>
+                        <p className='wikiSource'>Source: <a className='wikiSourceLink' href={data[content].source} target='_blank'>Wikipedia &#x2197;</a></p>
+                    </div>
+                </div>
+                <div className="controlsContainer">
+                    <h3 className={`controlItem ${selected === 1 ? 'selectedControlItem' : ''}`} onClick={() => {setOption(1)}}>O1 OVERVIEW</h3>
+                    <h3 className={`controlItem ${selected === 2 ? 'selectedControlItem' : ''}`} onClick={() => {setOption(2)}}>O2 INTERNAL STRUCTURE</h3>
+                    <h3 className={`controlItem ${selected === 3 ? 'selectedControlItem' : ''}`} onClick={() => {setOption(3)}}>O3 SURFACE GEOLOGY</h3>
+                </div>
+            </div>
             <div className='additionalDataContainer'>
-                <div className="rotation">
-                    <h4>ROTATION TIME</h4>
-                    <p>{data.rotation}</p>
+                <div className="additionalDataItem">
+                    <h4 className="additionalDataItemTitle">ROTATION TIME</h4>
+                    <p className="additionalDataItemInfo">{data.rotation}</p>
                 </div>
-                <div className="revolution">
-                    <h4>REVOLUTION TIME</h4>
-                    <p>{data.revolution}</p>
+                <div className="additionalDataItem">
+                    <h4 className="additionalDataItemTitle">REVOLUTION TIME</h4>
+                    <p className="additionalDataItemInfo">{data.revolution}</p>
                 </div>
-                <div className="radius">
-                    <h4>RADIUS</h4>
-                    <p>{data.radius}</p>
+                <div className="additionalDataItem">
+                    <h4 className="additionalDataItemTitle">RADIUS</h4>
+                    <p className="additionalDataItemInfo">{data.radius}</p>
                 </div>
-                <div className="temperature">
-                    <h4>AVERAGE TEMP</h4>
-                    <p>{data.temperature.toUpperCase()}</p>
+                <div className="additionalDataItem">
+                    <h4 className="additionalDataItemTitle">AVERAGE TEMP</h4>
+                    <p className="additionalDataItemInfo">{data.temperature.toUpperCase()}</p>
                 </div>
             </div>
         </>
